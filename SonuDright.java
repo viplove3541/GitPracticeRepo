49a0ff8c8d5933914f6326a974a3a54f8c56e694
SonuDright <144533703+SonuDright@users.noreply.github.com>
2026-03-23 16:23:36 +0530

    I Add  class

---
+++ SonuDright.java
@@ -0,0 +1,19 @@
+public class SonuDright {
+
+    void myName(){
+        System.out.println(" My Name is Sonu Dright");
+
+
+    }
+    void myEmail(){
+        System.out.println(" My Email is sonudright@gmail.com");
+
+
+    }
+    void myMobileNumber(){
+        System.out.println(" My Mobile Number 9631585973");
+
+    }
+
+
+}
